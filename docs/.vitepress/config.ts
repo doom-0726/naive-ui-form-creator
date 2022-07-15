daf178577eb3c85183b63e939605c7b3a2aeb742
doom-0726 <109345749+doom-0726@users.noreply.github.com>
2022-07-15 22:15:47 +0800

    doc

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,11 @@
 export default {
-  title: 'VitePress111',
-  description: 'Just playing around.',
+  title: 'naive-ui-pro-form',
+  description: '适用于NaiveUI的高级表单',
   themeConfig: {
     sidebar: [
       {
-        text: 'Guide',
-        items: [
-          { text: 'Introduction', link: '/introduction' },
-        ],
+        text: '介绍',
+        items: [{ text: '示例', link: '/introduction' }],
       },
     ],
   },
